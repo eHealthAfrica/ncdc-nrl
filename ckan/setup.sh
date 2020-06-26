c9aa8238855feea72d47e45a6e9d084f4383f440
lordmallam <lordy002000@gmail.com>
2020-06-26 07:51:41 +0100

    feat: ckan data request and gcs file upload

---
+++ ckan/setup.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2019 by eHealth Africa : http://www.eHealthAfrica.org
+#
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+set -Eeuo pipefail
+
+docker-compose -f ckan/docker-compose.yml kill
+docker-compose -f ckan/docker-compose.yml down
+
+docker-compose -f ckan/docker-compose.yml up -d
+CKAN_ID=$(docker-compose -f ckan/docker-compose.yml ps -q ckan)
+
+retries=1
+until docker exec -it $CKAN_ID /usr/local/bin/ckan-paster --plugin=ckan sysadmin -c /etc/ckan/production.ini add admin | tee creds.txt && echo "done"
+do
+    echo "waiting for ckan container to be ready... $retries"
+    sleep 5
+
+        ((retries++))
+        if [[ $retries -gt 30 ]]; then
+            echo "It was not possible to start CKAN"
+            exit 1
+        fi
+done
+
+docker-compose -f ckan/docker-compose.yml kill
+docker-compose -f ckan/docker-compose.yml down
